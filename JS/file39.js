0e42b5209c21b7cb069f329b3d106afe56b11dba
ChetanYadav34 <coc42chetan@gmail.com>
2026-02-16 13:43:24 +0530

    updated

---
+++ JS/file39.js
@@ -0,0 +1,17 @@
+// Hoisting with var
+// console.log(name)
+// var name = "John"
+// var name
+// console.log(name)
+// name = 'John'
+
+//// Hoisting with let and const
+// console.log(name)
+// let name = "John"
+
+// TDZ =
+
+// var name
+let name
+console.log(name)
+name = 'John'
