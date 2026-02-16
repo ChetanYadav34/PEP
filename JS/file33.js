b1ee94095b7e35cc9cc8517d97d71bc289f039bf
ChetanYadav34 <coc42chetan@gmail.com>
2026-02-16 10:40:32 +0530

    updates

--- JS/file33.js
+++ JS/file33.js
@@ -1,14 +1,37 @@
-const student = {
-  name: 'John',
-  age: 21,
-}
+// const student = {
+//   name: 'John',
+//   age: 21,
+// }
 
 // names = ['John', 'Catht', 'Mike']
 // const [name1, name2] = names
 // console.log(name1)
 // console.log(name2)
 
-const { name, age } = student
-console.log(name)
-console.log(age)
+// const { name, age } = student
+// console.log(name)
+// console.log(age)
 
+// const student = {
+//   name: 'John',
+//   age: 21,
+//   city: 'Jalandhar',
+// }
+// const { city = 'Amritsar' } = student
+// console.log(city)
+
+const student = {
+  name: 'John',
+  age: 21,
+  marks: {
+    math: 60,
+    science: 90,
+  },
+}
+
+const { marks } = student
+console.log(marks)
+const {
+  marks: { math },
+} = student
+console.log(math)
