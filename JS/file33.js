df82d8e7cea308f965ba2ab657417700c0640526
ChetanYadav34 <coc42chetan@gmail.com>
2026-02-16 10:27:11 +0530

    updated

---
+++ JS/file33.js
@@ -0,0 +1,14 @@
+const student = {
+  name: 'John',
+  age: 21,
+}
+
+// names = ['John', 'Catht', 'Mike']
+// const [name1, name2] = names
+// console.log(name1)
+// console.log(name2)
+
+const { name, age } = student
+console.log(name)
+console.log(age)
+
