5c7ce262e26f86a23570b9ed2244d4a6c3399c4e
ChetanYadav34 <coc42chetan@gmail.com>
2026-02-17 11:25:00 +0530

    updated

---
+++ JS/file48.js
@@ -0,0 +1,8 @@
+const students = [
+  { id: 1, name: 'Vansh', s1: true, s2: true,s3: false},
+  { id: 2, name: 'Vaishnav', s1: true, s2: true, s3: true},
+  { id: 2, name: 'Nikhil', s1: false, s2: true, s3: true},
+]
+
+// If id is 1 > Vansh is absent 
+
