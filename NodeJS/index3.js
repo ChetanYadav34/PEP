d20e990cb3c9c87c905a0855620d2b96acd8e8a4
ChetanYadav34 <coc42chetan@gmail.com>
2026-02-19 11:45:38 +0530

    updated

--- NodeJS/index3.js
+++ NodeJS/index3.js
@@ -4,11 +4,14 @@ app.listen(8080, () => {
   console.log('Server started')
 })
 app.get('/', (req, res) => {
+  console.log(req.url)
   res.send('Hi Baby')
 })
 app.get('/home', (req, res) => {
+  console.log(req.url)
   res.send('Hi Latent')
 })
 app.get('/home/page1', (req, res) => {
+  console.log(req.url)
   res.send('Hi Latent why Here')
 })
